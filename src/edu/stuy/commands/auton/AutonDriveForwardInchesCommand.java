b67037e757d408dda4e8fd30f69ce4cab523148a
Nicole Shin <shnnicole12321@yahoo.com>
2015-01-31 14:53:43 -0500

    Add ramping logic to AutonDriveForwardInchesCommand.

--- src/edu/stuy/commands/auton/AutonDriveForwardInchesCommand.java
+++ src/edu/stuy/commands/auton/AutonDriveForwardInchesCommand.java
@@ -1,8 +1,9 @@
 package edu.stuy.commands.auton;
 
-import edu.stuy.Robot;
-import edu.wpi.first.wpilibj.command.Command;
 import static edu.stuy.RobotMap.*;
+import edu.stuy.Robot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 /**
  *
@@ -10,7 +11,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class AutonDriveForwardInchesCommand extends Command {
 
     private double inches;
-    
+    private double startTime;
     
     /** 
      * If distance < 0, get it from SmartDashboard
@@ -26,6 +27,7 @@ public class AutonDriveForwardInchesCommand extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+        startTime = Timer.getFPGATimestamp();
         if (inches < 0) {
             inches = SmartDashboard.getNumber(INCHES_LABEL);
         }
@@ -34,7 +36,28 @@ public class AutonDriveForwardInchesCommand extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        Robot.drivetrain.tankDrive(1.0, 1.0);
+        double speed = getRampSpeed();
+        Robot.drivetrain.tankDrive(speed, speed);
+    }
+    
+    /**
+     * Ramping algorithm for linear acceleration and smooth velocity
+     * Takes one second to ramp up to max speed
+     * if t < 0.5, then speed = 2t^2;
+     * if t < 1, then speed = -2t^2 + 4t - 1;
+     * if t >= 1, then speed = 1;
+     * @return the speed the robot should go at the current time
+     */
+    
+    private double getRampSpeed() {
+        double t = Timer.getFPGATimestamp() - startTime;
+        if (t < 0.5) {
+            return 2 * t * t;
+        } else if (t < 1) {
+            return -2 * t * t + 4 * t - 1;
+        } else {
+            return 1;
+        }
     }
 
     // Make this return true when this Command no longer needs to run execute()
