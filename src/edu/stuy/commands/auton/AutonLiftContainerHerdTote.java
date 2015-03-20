8a9454447327086c5a348c068e193981f2f3f40a
Justin Kim <yulli@yulli.org>
2015-03-20 18:27:41 -0400

    Fix up AutonLiftContainerHerdTote and add important TODOs

--- src/edu/stuy/commands/auton/AutonLiftContainerHerdTote.java
+++ src/edu/stuy/commands/auton/AutonLiftContainerHerdTote.java
@@ -1,5 +1,6 @@
 package edu.stuy.commands.auton;
 
+import static edu.stuy.PhysicalConstants.TOTE_HEIGHT;
 import static edu.stuy.PhysicalConstants.TOTE_WIDTH;
 import static edu.stuy.RobotMap.AUTON_DRIVETRAIN_TOTE_WIDTH_TIMEOUT;
 import static edu.stuy.RobotMap.AUTON_DRIVE_FORWARD_DRIVER_SIDE_INCHES;
@@ -9,12 +10,18 @@ import edu.stuy.commands.LiftUpInchesCommand;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
- *
+ * The robot starts collinear with the auton set, with the hook in the can.
+ * 1) Lift up the can.
+ * 2) Drive forward and acquire the tote.
+ * 3) Turn 90° to the left.
+ * 4) Drive forward to the auton zone.
+ * 5) Drop the can onto the tote.
  */
 public class AutonLiftContainerHerdTote extends CommandGroup {
 
     public  AutonLiftContainerHerdTote() {
-        addSequential(new LiftUpInchesCommand(15.0));
+        // TODO: factor out TOTE_HEIGHT + 3
+        addSequential(new LiftUpInchesCommand(TOTE_HEIGHT + 3));
         addParallel(new AutonAcquirerAcquireCommand());
         addSequential(new AutonDriveForwardInchesCommand(TOTE_WIDTH , AUTON_DRIVETRAIN_TOTE_WIDTH_TIMEOUT));
         addSequential(new AutonAcquirerOffCommand());
@@ -22,5 +29,8 @@ public class AutonLiftContainerHerdTote extends CommandGroup {
         addSequential(new DrivetrainRotateCommand(-90) , 2.0);
         // Go to auton zone
         addSequential(new AutonDriveForwardInchesCommand(AUTON_DRIVE_FORWARD_DRIVER_SIDE_INCHES, AUTON_DRIVE_FORWARD_DRIVER_SIDE_TIMEOUT));
+        // FIXME: lower the can
+        // TODO: factor out TOTE_HEIGHT + 3
+        // addSequential(new LiftDownInchesCommand(TOTE_HEIGHT + 3));
     }
 }
