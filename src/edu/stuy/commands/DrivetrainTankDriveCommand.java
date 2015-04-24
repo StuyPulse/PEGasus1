ec9abcf9fc724a2bfa3172d5ba641e11efee52b2
elc1798 <subliminalmau5@gmail.com>
2015-04-24 16:44:13 -0400

    Remove bad ramping from canner branch for book sale

--- src/edu/stuy/commands/DrivetrainTankDriveCommand.java
+++ src/edu/stuy/commands/DrivetrainTankDriveCommand.java
@@ -26,11 +26,6 @@ public class DrivetrainTankDriveCommand extends Command {
     protected void execute() {
         double left = Robot.oi.driverPad.getLeftY();
         double right = Robot.oi.driverPad.getRightY();
-
-        if (Math.abs(left) < .05 && Math.abs(right) < .05) {
-            startTime = Timer.getFPGATimestamp();
-        }
-
         if (Robot.oi.driverPad.getRawLeftTrigger() || Robot.oi.driverPad.getRawLeftBumper()) {
             Robot.drivetrain.setSpeedUp(false);
         }
@@ -39,31 +34,10 @@ public class DrivetrainTankDriveCommand extends Command {
         }
 
         if (Robot.drivetrain.isSpeedUp()) {
-            if ((left < 0 && right > 0) || (left > 0 && right < 0)) { // Rotation does not use ramping
-                Robot.drivetrain.tankDrive(-left, -right);
-            } else {
-                Robot.drivetrain.tankDrive(getRampSpeed(-left), getRampSpeed(-right));
-            }
+            Robot.drivetrain.tankDrive(-left, -right);
         } else {
-            if ((left < 0 && right > 0) || (left > 0 && right < 0)) { // Rotation does not use ramping
-                Robot.drivetrain.tankDrive(-left, -right);
-            } else {
-                Robot.drivetrain.tankDrive(getRampSpeed(-left) * DRIVETRAIN_SLOWNESS_FACTOR, getRampSpeed(-right) * DRIVETRAIN_SLOWNESS_FACTOR);
-            }
+            Robot.drivetrain.tankDrive(-left * DRIVETRAIN_SLOWNESS_FACTOR, -right * DRIVETRAIN_SLOWNESS_FACTOR);
         }
-    }
-
-    private double getRampSpeed(double m) {
-        double t = Timer.getFPGATimestamp() - startTime;
-        double speed;
-        if (t < 0.5) {
-            speed = 2 * t * t;
-        } else if (t < 1) {
-            speed = -2 * t * t + 4 * t - 1;
-        } else {
-            speed = 1;
-        }
-        return /* speed * */ m;
     }
 
     // Make this return true when this Command no longer needs to run execute()
