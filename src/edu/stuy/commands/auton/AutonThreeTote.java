7ed72c59ab44e0fb9f20107ca47e6442ce0abf85
IshraqBhuiyan <ishraqbhuiyan@gmail.com>
2015-05-11 17:35:37 -0400

    Make progress on three tote auton.

--- src/edu/stuy/commands/auton/AutonThreeTote.java
+++ src/edu/stuy/commands/auton/AutonThreeTote.java
@@ -1,6 +1,10 @@
 package edu.stuy.commands.auton;
 
+import static edu.stuy.PhysicalConstants.*;
+import edu.stuy.commands.ArmsSetNarrowCommand;
 import edu.stuy.commands.DrivetrainRotateNoPIDCommand;
+import edu.stuy.commands.LiftDownInchesCommand;
+import edu.stuy.commands.LiftUpInchesCommand;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
@@ -9,6 +13,20 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class AutonThreeTote extends CommandGroup {
     
     public  AutonThreeTote() {
+        addParallel(new AutonAcquirerAcquireCommand(), 15);
         addSequential(new DrivetrainRotateNoPIDCommand(30));
+        addSequential(new AutonDriveForwardInchesCommand(10, 2));
+        addSequential(new DrivetrainRotateNoPIDCommand(-30));
+        addSequential(new AutonDriveForwardInchesCommand(50, 5));
+        addSequential(new LiftUpInchesCommand(RECYCLING_BIN_HEIGHT+TOTE_HEIGHT));
+        addSequential(new AutonDriveForwardInchesCommand(24, 3));
+        addSequential(new AutonDriveBackwardInchesCommand(4, 3));
+        addSequential(new LiftDownInchesCommand(RECYCLING_BIN_HEIGHT + TOTE_HEIGHT));
+        addSequential(new ArmsSetNarrowCommand());
+        addSequential(new AutonDriveForwardInchesCommand(24, 3));
+        addSequential(new AutonDriveBackwardInchesCommand(4, 3));
+        addSequential(new LiftDownInchesCommand(RECYCLING_BIN_HEIGHT + TOTE_HEIGHT));
+        addSequential(new AutonDriveForwardInchesCommand(10, 3));
+        addSequential(new ArmsSetNarrowCommand());
     }
 }
